32b1b485abbb5aca5b30c13b503a7f1a84c7eef8
Kav Singh <flakisms@gmail.com>
2017-04-16 18:28:58 +1000

    scroll masking.

--- client/components/ShrineQuestList/index.js
+++ client/components/ShrineQuestList/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import { negate, filter } from 'lodash/fp'
 import ShrineQuest from '../ShrineQuest'
@@ -7,15 +7,6 @@ import background from './oman-au.jpg'
 const isComplete = quest => !!quest.complete
 const getComplete = filter(isComplete)
 const getIncomplete = filter(negate(isComplete))
-
-const renderQuest = (onClick, props) => (
-  <ShrineQuest
-    {...props}
-    onClick={() => onClick(props)}
-    key={props.id}
-    style={{ margin: '0 0 0.6em 0' }}
-  />
-)
 
 const gradientMaskBottom =
   `linear-gradient(
@@ -42,107 +33,149 @@ const gradientMaskBoth =
     rgba(0, 0, 0, 0)
   )`
 
-export default function ShrineQuestList({
-  shrineQuests,
-  onItemClick,
-  group,
-}) {
-  const renderer = renderQuest.bind(null, onItemClick)
-  const completeQuests = getComplete(shrineQuests)
-  const list = group
-    ? getIncomplete(shrineQuests).concat(completeQuests)
-    : shrineQuests
+export default class ShrineQuestList extends PureComponent {
+  constructor(...args) {
+    super(...args)
+    this.state = { scrollClass: 'scrollTop' }
+    this.renderItem = this.renderItem.bind(this)
+    this.handleScroll = this.handleScroll.bind(this)
+  }
 
-  return (
-    <div className="root">
-      <div className="content">
-        <div className="stats">
-          <div className="remaining">
-            {`${shrineQuests.length - completeQuests.length} remaining`}
+  componentDidMount() {
+    if (!this.listContainer) return
+    this.listContainer.addEventListener('scroll', this.handleScroll, false)
+  }
+
+  componentWillUnmount() {
+    if (!this.listContainer) return
+    this.listContainer.removeEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll({ currentTarget } = {}) {
+    if (!currentTarget) return
+
+    const { scrollTop, offsetHeight } = currentTarget
+    const { offsetHeight: itemsHeight } = this.itemsContainer
+    const maxScrollHeight = itemsHeight - offsetHeight
+    let scrollClass = 'scrollMiddle'
+
+    if (scrollTop <= 0) scrollClass = 'scrollTop'
+    else if (scrollTop >= maxScrollHeight) scrollClass = 'scrollBottom'
+
+    this.setState(() => ({ scrollClass }))
+  }
+
+  renderItem(props) {
+    return (
+      <ShrineQuest
+        {...props}
+        onClick={() => this.props.onItemClick(props)}
+        key={props.id}
+        style={{ margin: '0 0 0.6em 0' }}
+      />
+    )
+  }
+
+  render() {
+    const { shrineQuests, group } = this.props
+    const { scrollClass } = this.state
+    const completeQuests = getComplete(shrineQuests)
+    const list = group
+      ? getIncomplete(shrineQuests).concat(completeQuests)
+      : shrineQuests
+
+    return (
+      <div className="root">
+        <div className="content">
+          <div className="stats">
+            <div className="remaining">
+              {`${shrineQuests.length - completeQuests.length} remaining`}
+            </div>
+            <div className="counts">
+              <span className="completed">{
+                completeQuests.length
+              }</span> / <span className="total">{
+                shrineQuests.length
+              }</span> complete
+            </div>
           </div>
-          <div className="counts">
-            <span className="completed">{
-              completeQuests.length
-            }</span> / <span className="total">{
-              shrineQuests.length
-            }</span> complete
+          <div
+            className={`listContainer ${scrollClass}`}
+            ref={c => { this.listContainer = c }}
+          >
+            <div ref={c => { this.itemsContainer = c }}>
+              {list.map(this.renderItem)}
+            </div>
           </div>
         </div>
-        <div className="listContainer scrollTop">
-          {list.map(renderer)}
-        </div>
+        <div className="background" />
+        <style jsx>{`
+          .root {
+            width: 100%;
+            height: 100%;
+          }
+          .content,
+          .background {
+            position: absolute;
+          }
+          .content {
+            display: flex;
+            flex-direction: column;
+            padding: 1em;
+            z-index: 2;
+            top: 6vmin;
+            right: 6vmin;
+            bottom: 6vmin;
+            left: 6vmin;
+            background-color: rgba(255, 255, 255, 0.1);
+            border: 2px solid rgba(255, 255, 255, 0.16);
+          }
+          .stats {
+            display: flex;
+            align-items: center;
+            flex: 0 0 auto;
+            padding: 1em 1.6em;
+          }
+          .remaining {
+            color: white;
+            font-weight: 600;
+            font-size: 1.2em;
+            margin-right: 0.8em;
+          }
+          .counts .completed {
+            color: white;
+          }
+          .listContainer {
+            flex: 1 0;
+            padding: 1em;
+            overflow-x: visible;
+            overflow-y: scroll;
+          }
+          .listContainer.scrollTop {
+            mask-image: ${gradientMaskBottom};
+            -webkit-mask-image: ${gradientMaskBottom};
+          }
+          .listContainer.scrollMiddle {
+            mask-image: ${gradientMaskBoth};
+            -webkit-mask-image: ${gradientMaskBoth};
+          }
+          .listContainer.scrollBottom {
+            mask-image: ${gradientMaskTop};
+            -webkit-mask-image: ${gradientMaskTop};
+          }
+          .background {
+            background: url('${background}') no-repeat 60% center;
+            background-size: cover;
+            filter: blur(3px);
+            top: -3px;
+            right: -3px;
+            bottom: -3px;
+            left: -3px;
+          }
+        `}</style>
       </div>
-      <div className="background" />
-      <style jsx>{`
-        .root {
-          width: 100%;
-          height: 100%;
-        }
-        .content,
-        .background {
-          position: absolute;
-        }
-        .content {
-          display: flex;
-          flex-direction: column;
-          padding: 1em;
-          z-index: 2;
-          top: 6vmin;
-          right: 6vmin;
-          bottom: 6vmin;
-          left: 6vmin;
-          background-color: rgba(255, 255, 255, 0.1);
-          border: 2px solid rgba(255, 255, 255, 0.16);
-        }
-        .stats {
-          display: flex;
-          align-items: center;
-          flex: 0 0 auto;
-          padding: 1em 1.6em;
-        }
-        .remaining {
-          color: white;
-          font-weight: 600;
-          font-size: 1.2em;
-          margin-right: 0.8em;
-        }
-        .counts .completed {
-          color: white;
-        }
-        .listContainer {
-          flex: 1 0;
-          padding: 1em;
-          overflow-x: visible;
-          overflow-y: scroll;
-        }
-        .listContainer.scrollTop {
-          mask-image: ${gradientMaskBottom};
-          -webkit-mask-image: ${gradientMaskBottom};
-        }
-        .listContainer.scrollMiddle {
-          mask-image: ${gradientMaskBoth};
-          -webkit-mask-image: ${gradientMaskBoth};
-        }
-        .listContainer.scrollBottom {
-          mask-image: ${gradientMaskTop};
-          -webkit-mask-image: ${gradientMaskTop};
-        }
-        .background {
-          background: url('${background}') no-repeat 60% center;
-          background-size: cover;
-          filter: blur(3px);
-          top: -3px;
-          right: -3px;
-          bottom: -3px;
-          left: -3px;
-        }
-      `}</style>
-    </div>
-  )
-}
-
-renderQuest.propTypes = {
-  id: PropTypes.string.isRequired,
+    )
+  }
 }
 
 ShrineQuestList.propTypes = {
