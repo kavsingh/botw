644f22434001f82822d41999fdf43b637ac8dc6c
Kav Singh <flakisms@gmail.com>
2017-04-19 22:14:40 +1000

    Stats to own component.

---
+++ client/components/CompletionStats/index.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default function CompletionStats({ totalCount, completedCount }) {
+  return (
+    <div className="root">
+      <div className="remaining">{totalCount - completedCount}</div>
+      <div className="counts">
+        <span className="completed">{
+          completedCount
+        }</span> / <span className="total">{
+          totalCount
+        }</span> complete
+      </div>
+      <style jsx>{`
+        .root {
+          display: flex;
+          align-items: center;
+          padding: 1em 1.6em;
+        }
+        .remaining {
+          color: white;
+          font-weight: 600;
+          font-size: 1.2em;
+          margin-right: 0.8em;
+        }
+        .counts .completed {
+          color: white;
+        }
+      `}</style>
+    </div>
+  )
+}
+
+CompletionStats.propTypes = {
+  totalCount: PropTypes.number,
+  completedCount: PropTypes.number,
+}
+
+CompletionStats.defaultProps = {
+  totalCount: 0,
+  completedCount: 0,
+}
