940c5d4a0e1adac2aeba3633acd15036409302bb
Kav Singh <flakisms@gmail.com>
2017-04-26 17:29:04 +1000

    Script for shrine pop.

---
+++ server/scripts/populateShrines.js
@@ -0,0 +1,54 @@
+/* eslint-disable import/no-dynamic-require */
+
+const Promise = require('bluebird')
+const path = require('path')
+const fs = Promise.promisifyAll(require('fs'))
+const { has } = require('lodash/fp')
+
+const dataPath = path.resolve(__dirname, '../data')
+const shrinesPath = path.resolve(dataPath, 'shrines')
+const questsPath = path.resolve(dataPath, 'shrineQuests')
+
+const shrineData = require(shrinesPath)
+const questData = require(questsPath)
+
+const questShrines = Object.values(questData)
+  .map(({ id, shrines }) => shrines.reduce((o, shrine) => ({
+    id: shrine,
+    shrineQuest: id,
+  }), {}))
+
+const newShrines = Object.values(shrineData)
+  .map(shrine => {
+    const hasAttr = has(has.placeholder, shrine)
+    return Object.assign(shrine, {
+      type: hasAttr('type') ? shrine.type : '',
+      shrineQuests: hasAttr('shrineQuests') ? shrine.shrineQuests : [],
+    })
+  })
+  .reduce((shrines, shrine) =>
+    Object.assign(shrines, { [shrine.id]: shrine }), {})
+
+questShrines.forEach(({ id, shrineQuest }) => {
+  const shrine = newShrines[id]
+
+  if (!shrine) {
+    newShrines[id] = {
+      id,
+      name: '',
+      region: '',
+      type: '',
+      location: '',
+      shrineQuests: [shrineQuest],
+    }
+
+    return
+  }
+
+  if (!(shrine.shrineQuests || []).includes(shrineQuest)) {
+    shrine.shrineQuests = [shrineQuest]
+  }
+})
+
+fs.writeFileAsync(
+  `${shrinesPath}--updated.json`, JSON.stringify(shrineData, null, 2))
