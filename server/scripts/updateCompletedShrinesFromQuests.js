e9e8d89dfc3191cd2f7780b7d06b6f7f154bc263
Kav Singh <flakisms@gmail.com>
2017-04-26 17:50:30 +1000

    update completed shrines from shrine quests.

---
+++ server/scripts/updateCompletedShrinesFromQuests.js
@@ -0,0 +1,27 @@
+/* eslint-disable import/no-dynamic-require */
+
+const Promise = require('bluebird')
+const path = require('path')
+const fs = Promise.promisifyAll(require('fs'))
+const { flatMap, get, union, filter, pipe } = require('lodash/fp')
+
+const dataPath = path.resolve(__dirname, '../data')
+const statsPath = path.resolve(dataPath, 'stats')
+const questsPath = path.resolve(dataPath, 'shrineQuests')
+
+const statsData = require(statsPath)
+const questData = require(questsPath)
+
+const { completedShrines, completedShrineQuests } = statsData
+
+const completedFromQuests = pipe(
+  filter(({ id }) => completedShrineQuests.includes(id)),
+  flatMap(get('shrines'))
+)(Object.values(questData))
+
+const newStats = Object.assign(statsData, {
+  completedShrines: union(completedShrines, completedFromQuests),
+})
+
+fs.writeFileAsync(
+  `${statsPath}--updated.json`, JSON.stringify(newStats, null, 2))
