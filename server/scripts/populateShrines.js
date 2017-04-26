f657b3f002d9c9cfc6d664ef5be054736b6bad8e
Kav Singh <flakisms@gmail.com>
2017-04-27 03:33:09 +1000

    Order shrines by region.

--- server/scripts/populateShrines.js
+++ server/scripts/populateShrines.js
@@ -3,48 +3,18 @@
 const Promise = require('bluebird')
 const path = require('path')
 const fs = Promise.promisifyAll(require('fs'))
-const { has } = require('lodash/fp')
+const { groupBy, flatten } = require('lodash/fp')
 
 const dataPath = path.resolve(__dirname, '../data')
 const shrinesPath = path.resolve(dataPath, 'shrines')
-const questsPath = path.resolve(dataPath, 'shrineQuests')
 
 const shrineData = require(shrinesPath)
-const questData = require(questsPath)
 
-const newShrines = Object.values(shrineData)
-  .map(shrine => {
-    const hasAttr = has(has.placeholder, shrine)
-    return Object.assign(shrine, {
-      type: hasAttr('type') ? shrine.type : '',
-      shrineQuests: hasAttr('shrineQuests') ? shrine.shrineQuests : [],
-    })
-  })
-  .reduce((shrines, shrine) =>
-    Object.assign(shrines, { [shrine.id]: shrine }), {})
-
-const questShrines = Object.values(questData)
-  .map(({ id, shrines }) => shrines.reduce((o, shrine) => ({
-    id: shrine,
-    shrineQuest: id,
-  }), {}))
-
-questShrines.forEach(({ id, shrineQuest }) => {
-  const shrine = newShrines[id]
-
-  if (!shrine) {
-    newShrines[id] = {
-      id,
-      name: '',
-      region: '',
-      type: '',
-      location: '',
-      shrineQuests: [shrineQuest],
-    }
-  } else if (!(shrine.shrineQuests || []).includes(shrineQuest)) {
-    shrine.shrineQuests = [shrineQuest]
-  }
-})
+const newShrineData = flatten(Object.values(groupBy('region', shrineData)))
+  .reduce((result, shrine) => {
+    result[shrine.id] = shrine
+    return result
+  }, {})
 
 fs.writeFileAsync(
-  `${shrinesPath}--updated.json`, JSON.stringify(newShrines, null, 2))
+  `${shrinesPath}--updated.json`, JSON.stringify(newShrineData, null, 2))
